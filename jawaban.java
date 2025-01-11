9b49294b888056993838443624ef60fe2064b335
Hanief Bintang Tri Buana <haniefbintang3@gmail.com>
2025-01-11 21:28:28 +0700

    Create jawaban.java

---
+++ jawaban.java
@@ -0,0 +1,28 @@
+public class jawaban {
+    public static void main(String[] args) {
+        // tipe data primitif
+        byte tipeByte = 127;
+        short tipeShort = 32000; 
+        int tipeInt = 2147483647;
+        long tipeLong = 9223372036854775807L; 
+        float tipeFloat = 3.14f; 
+        double tipeDouble = 3.141592653589793; 
+        char tipeChar = 'A'; 
+        boolean tipeBoolean = true;
+
+        // tipe data referensi
+        String tipeString = "Ini contoh tipe data String!";
+
+        // menampilkan nilai dari tiap tipe data
+        System.out.println("Contoh Tipe Data di Java:");
+        System.out.println("1. byte    : " + tipeByte);
+        System.out.println("2. short   : " + tipeShort);
+        System.out.println("3. int     : " + tipeInt);
+        System.out.println("4. long    : " + tipeLong);
+        System.out.println("5. float   : " + tipeFloat);
+        System.out.println("6. double  : " + tipeDouble);
+        System.out.println("7. char    : " + tipeChar);
+        System.out.println("8. boolean : " + tipeBoolean);
+        System.out.println("9. String  : " + tipeString);
+    }
+}
